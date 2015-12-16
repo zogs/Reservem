42fb351fa95ad3e7a9be757bad816e31bff25d71
Simon <guichardsim@gmail.com>
2015-12-16 10:40:00 +0100

    debug  barcode detector

--- src/Crc/ReservBundle/Resources/public/js/main.js
+++ src/Crc/ReservBundle/Resources/public/js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
 	.bind('scannerDetectionComplete',function(e,data){ 
 
 		action = $('#barcode').attr('data-action');
-		reservid = $('#barcode').attr('data-reservation-id');
-		barcode = data.string.replace(/à/g, '0').replace(/&/g, '1').replace(/é/g,'2').replace(/\"/g,'3').replace(/\'/g,'4').replace(/\(/g,'5').replace(/\-/g,'6').replace(/è/g,'7').replace(/\_/g,'8').replace(/ç/g,'9');
-
+		reservid = $('#barcode').attr('data-reservation-id');						
+		barcode = data.string;
+		
 		if(action == 'takeout') {
 
 			takeoutDevice(barcode,reservid);
@@ -23,9 +23,9 @@ $(document).ready(function() {
 	.bind('scannerDetectionError',function(e,data){
 
 		action = $('#barcode').attr('data-action');
-		reservid = $('#barcode').attr('data-reservation-id');
-		barcode = data.string.replace(/à/g, '0').replace(/&/g, '1').replace(/é/g,'2').replace(/\"/g,'3').replace(/\'/g,'4').replace(/\(/g,'5').replace(/\-/g,'6').replace(/è/g,'7').replace(/\_/g,'8').replace(/ç/g,'9');
-
+		reservid = $('#barcode').attr('data-reservation-id');						
+		barcode = data.string;
+		
 		if(action == 'takeout') {
 
 			takeoutDevice(barcode,reservid);
@@ -36,7 +36,9 @@ $(document).ready(function() {
 		}
 
 	})
-	.bind('scannerDetectionReceive',function(e,data){ console.log('receive'); })
+	.bind('scannerDetectionReceive',function(e,data){ 
+		console.log('receive'); 		
+	})
 	;
 });
 
